1b6f524ece9ab80651e0e35f0ec35fe33b303de7
John-Wambua <jwambua307@gmail.com>
2020-07-17 12:41:42 +0300

    first commit

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      id="1"
+      thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS_rbqRlg_dukeGDrtkk3R5Lfb_wsjjxd7how&usqp=CAU"
+      title="Hello There!"
+      previewText="My first Post"
+    />
+    <PostPreview
+      id="2"
+      thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS_rbqRlg_dukeGDrtkk3R5Lfb_wsjjxd7how&usqp=CAU"
+      title="Wassupp!"
+      previewText="My second Post"
+    />
+    <PostPreview
+      id="3"
+      thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS_rbqRlg_dukeGDrtkk3R5Lfb_wsjjxd7how&usqp=CAU"
+      title="How are you my guy!"
+      previewText="My third Post"
+    />
+    <PostPreview
+      id="4"
+      thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS_rbqRlg_dukeGDrtkk3R5Lfb_wsjjxd7how&usqp=CAU"
+      title="Is How?"
+      previewText="My Fourth Post"
+    />
+
+  </section>
+</template>
+
+<script>
+    import PostPreview from "./PostPreview";
+    export default {
+        name: "PostList",
+      components: {PostPreview}
+    }
+</script>
+
+<style scoped>
+
+  .post-list {
+    display: flex;
+    padding: 20px;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="posts-page">
+    <PostList/>
+  </div>
+</template>
+
+<script>
+    import PostPreview from "../../components/Posts/PostPreview";
+    import PostList from "../../components/Posts/PostList";
+    export default {
+        name: "index",
+      components: {PostList, PostPreview}
+    }
+</script>
+
+<style scoped>
+  .posts-page{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
